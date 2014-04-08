e15229b38c74b669b72521e9e2e8fc252715e8cb
sebastien <sebastien@ferre.io>
2014-04-08 13:03:33 +0200

    full_name

--- 01-Ruby/01-Programming-basics/04-What-your-name/lib/interface.rb
+++ 01-Ruby/01-Programming-basics/04-What-your-name/lib/interface.rb
@@ -1,9 +1,8 @@
 require_relative "compute_name"
 
 def ask_and_get(param)
-  #TODO:
-  #- Ask the user to write a value for param
-  #- Get and return the string inputted by the user
+  puts "What is your " + param
+  param = gets.chomp
 end
 
 def name_from_terminal
@@ -18,6 +17,6 @@ full_name = name_from_terminal
 
 #TODO: use string concatenation to build a custom message embedding the computed full_name
 
-# custom_message =
+custom_message = "Your name is #{full_name} and its length is #{full_name.length} characters."
 
 puts custom_message
