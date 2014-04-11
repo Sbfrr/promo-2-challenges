2490a3c8440f193266ef592f8cd9fea9b034c84b
sebastien <sebastien@ferre.io>
2014-04-11 11:46:07 +0200

    LOUCHEBELEM changes

--- 01-Ruby/03-Blocks/05-Louchebem/lib/louchebem.rb
+++ 01-Ruby/03-Blocks/05-Louchebem/lib/louchebem.rb
@@ -1,12 +1,7 @@
 # Encoding: utf-8
-# constraint: you should pick your final random suffix in ["em", "é", "ji", "oc", "ic", "uche", "ès"]
-# constraint: one-letter word like "a" or "l" should not be translated
-# constraint: word beginning with a vowel should be translated differently. Ex: "atout" may give "latoutoc"
-# enhancement: ideally your program should be able to translate any complicated sentence, eventually embedding special characters like "!", or "'".
 
 SUFFIX = ["em", "é", "ji", "oc", "ic", "uche", "ès"]
 CONSONNANTS = ["b", "c" ,"d" ,"f" ,"g" ,"h" ,"j" ,"k" ,"l" ,"m" ,"n" ,"p" ,"q" ,"r" ,"s" ,"t" ,"v" ,"w" ,"x" , "z"]
-SPECIAL = [",", ";", ":", ".", "?", "!", "\"", "(", ")", "-", "_" ]
 
 def louchebemize_word(word)
 
@@ -17,8 +12,7 @@ def louchebemize_word(word)
   unless length == 1
     if CONSONNANTS.include? (first.downcase)
       if CONSONNANTS.include? (second.downcase)
-        word[0] = ""
-        word[0] = ""
+        word[0..1] = ""
         "l#{word}#{first + second}#{SUFFIX[rand(6)]}"
       else
         word[0] = ""
