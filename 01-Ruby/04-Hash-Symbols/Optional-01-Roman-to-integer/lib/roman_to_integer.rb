bc4e66a860bc5020af03b90b98889b2a5ed15b3b
sebastien <sebastien@ferre.io>
2014-04-11 16:22:21 +0200

    New roman, the complex way

--- 01-Ruby/04-Hash-Symbols/Optional-01-Roman-to-integer/lib/roman_to_integer.rb
+++ 01-Ruby/04-Hash-Symbols/Optional-01-Roman-to-integer/lib/roman_to_integer.rb
@@ -1,9 +1,54 @@
-def roman_to_integer(roman_string)
+require 'pry'
+
+ROMAN = {
+
+  "IV" => 4,
+  "IX" => 9,
+  "XL" => 40,
+  "XC" => 90,
+  "CD" => 400,
+  "CM" => 900,
+  "I" => 1,
+  "V" => 5,
+  "X" => 10,
+  "L" => 50,
+  "C" => 100,
+  "D" => 500,
+  "M" => 1000
+}
+
+
+
+
+def roman_to_integer(string)
   #TODO: translate roman string to integer
+
+  integer = 0
+  until string == "" do
+
+    ROMAN.each_key do |key|
+      if string[0..1] == key
+        integer += ROMAN[key]
+        string[0..1] = ""
+      end
+    end
+
+    ROMAN.each_key do |key|
+      if string[0] == key
+        integer += ROMAN[key]
+        string[0] = ""
+      end
+    end
+
+  end
+  integer
+
 end
 
 # testing your program
 puts roman_to_integer('IV') == 4 # => true
 puts roman_to_integer('XII') == 12 # => true
 puts roman_to_integer('XIX') == 19 # => true
+
+#I = 1 - V = 5 - X = 10 - L = 50 - C = 100 - D = 500 - M = 1000
 
