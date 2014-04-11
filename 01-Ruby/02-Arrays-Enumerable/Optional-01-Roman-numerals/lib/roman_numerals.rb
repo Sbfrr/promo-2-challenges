7d8de41ba53c62d02e61502c8a428f324fd1d016
sebastien <sebastien@ferre.io>
2014-04-11 16:56:45 +0200

    roman V2

--- 01-Ruby/02-Arrays-Enumerable/Optional-01-Roman-numerals/lib/roman_numerals.rb
+++ 01-Ruby/02-Arrays-Enumerable/Optional-01-Roman-numerals/lib/roman_numerals.rb
@@ -27,35 +27,43 @@ end
 def new_roman_numeral(integer)
 
   #TODO: translate integer in roman number - old-style way
-  values = [1000, 500, 100, 50, 10, 5, 1]
-  letters = ["M", "D", "C", "L", "X", "V", "I"]
+  values = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+  letters = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
   builder_index = []
   solution = Array.new
 
   index = 0
-  until index == 7 do
+  until index == 13 do
     builder_index << integer/values[index]
     integer = integer % values[index]
     index += 1
   end
 
   index = 0
-  until index >= 7 do
-    if builder_index[index] == 1 && builder_index[index + 1] == 4
-      solution << letters[index + 1]
-      solution << letters[index - 1]
-      index += 2
-    elsif builder_index[index] == 4
-      solution << letters[index]
-      solution << letters[index-1]
-      index += 2
-    else
-      solution << letters[index]*builder_index[index]
-      index += 1
-    end
+  until index == 13 do
+    solution << letters[index]*builder_index[index]
+    index += 1
   end
-
   solution.join
+
+
+  # index = 0
+  # until index >= 7 do
+  #   if builder_index[index] == 1 && builder_index[index + 1] == 4
+  #     solution << letters[index + 1]
+  #     solution << letters[index - 1]
+  #     index += 2
+  #   elsif builder_index[index] == 4
+  #     solution << letters[index]
+  #     solution << letters[index-1]
+  #     index += 2
+  #   else
+  #     solution << letters[index]*builder_index[index]
+  #     index += 1
+  #   end
+  # end
+
+  # solution.join
 
 end
 
