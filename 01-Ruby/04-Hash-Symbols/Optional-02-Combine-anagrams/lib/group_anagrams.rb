19786a8336aad8f90336bca40e68028fc4c6fe48
sebastien <sebastien@ferre.io>
2014-04-11 16:49:27 +0200

    group anagrams

--- 01-Ruby/04-Hash-Symbols/Optional-02-Combine-anagrams/lib/group_anagrams.rb
+++ 01-Ruby/04-Hash-Symbols/Optional-02-Combine-anagrams/lib/group_anagrams.rb
@@ -1,6 +1,22 @@
-def group_anagrams(words)
-  #TODO: group anagrams
+def sorted(word)
+  word.chars.sort.join.downcase
 end
+
+def group_anagrams(words)
+
+  anagrams = {}
+  anagrams_array = []
+
+  words.each do |word|
+    anagrams[sorted(word)] ? anagrams[sorted(word)] << word : anagrams[sorted(word)] = [word]
+  end
+
+  anagrams.each do |key, value |
+    anagrams_array << value
+  end
+
+end
+
 
 # input:
 p group_anagrams( ['cars', 'for', 'potatoes', 'racs', 'four','scar', 'creams', 'scream'] )
