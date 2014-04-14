246c4f4a8afe2b5678780792721ade9e2564f818
sebastien <sebastien@ferre.io>
2014-04-14 15:18:05 +0200

    Mail joke

--- 01-Ruby/05-Regular-Expressions/03-Mail-spotter/lib/mail_joke.rb
+++ 01-Ruby/05-Regular-Expressions/03-Mail-spotter/lib/mail_joke.rb
@@ -1,5 +1,27 @@
 # Encoding: utf-8
+require 'pry'
 
 def mail_joke(email)
-  # TODO: Return a joke suited to the email provided
+
+  raise ArgumentError, 'Argument is not an email' if email.match(/@/) == nil
+
+
+  adress = email.match(/(?<name>\w*)\.?+(?<last_name>\w*)@(?<domain>\w*)/)
+
+  case adress[:domain]
+  when "lewagon"
+    "Well done #{adress[:name]}, you're skilled and capable"
+  when "gmail"
+    "#{adress[:name]}, you're an average but modern person"
+  when "live"
+    "jean-#{adress[:name]} #{adress[:last_name]}, aren't you born before 1973?"
+  else
+    "Sorry #{adress[:name]}, we don't know how to judge '#{adress[:domain]}.gal'"
+  end
+
+
+
+
 end
+
+puts mail_joke("seb@lewagon")
