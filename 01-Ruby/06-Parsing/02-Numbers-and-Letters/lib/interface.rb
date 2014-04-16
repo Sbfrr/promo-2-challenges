148ded9456f81f0467357de25ae114ba9b5bfdcf
sebastien <sebastien@ferre.io>
2014-04-16 12:08:25 +0200

    update

--- 01-Ruby/06-Parsing/02-Numbers-and-Letters/lib/interface.rb
+++ 01-Ruby/06-Parsing/02-Numbers-and-Letters/lib/interface.rb
@@ -1,4 +1,15 @@
-require_relative "longest_word"
+require_relative 'longest_word'
+
+require 'socket'                # Get sockets from stdlib
+
+server = TCPServer.open(2000)   # Socket to listen on port 2000
+loop {                          # Servers run forever
+  Thread.start(server.accept) do |client|
+    client.puts(Time.now.ctime) # Send the time to the client
+  client.puts "Closing the connection. Bye!"
+    client.close                # Disconnect from the client
+  end
+}
 
 puts "******** Welcome to the longest word-game !********"
 puts "Here is your grid :"
