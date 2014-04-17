e8e6cd35347288ccf7c34565ec60923f87645d4f
sebastien <sebastien@ferre.io>
2014-04-17 11:44:37 +0200

    dessert.rb

--- 02-OOP/05-Dessert-inheritance/lib/dessert.rb
+++ 02-OOP/05-Dessert-inheritance/lib/dessert.rb
@@ -1,15 +1,32 @@
 class Dessert
+
+  attr_accessor :name, :calories
+
   def initialize(name, calories)
-  end 
-  
-  def healthy?
+    @calories = calories
+    @name = name
   end
-  
+
+  def healthy?
+    @calories < 200 ? true : false
+  end
+
   def delicious?
+    true
   end
 end
 
-class JellyBean < Dessert  
+class JellyBean < Dessert
+
+  attr_reader :flavor
+
   def initialize(name, calories, flavor)
+    super(name, calories)
+    @flavor = flavor
   end
+
+  def delicious?
+    @flavor == "black licorice" ? false : true
+  end
+
 end
