b17404fce3647ba3daa0b561a4b410c520c255a5
sebastien <sebastien@ferre.io>
2014-04-17 15:03:52 +0200

    restaurants

--- 02-OOP/06-Instance-vs-Class/lib/restaurant.rb
+++ 02-OOP/06-Instance-vs-Class/lib/restaurant.rb
@@ -1,11 +1,27 @@
 class Restaurant
-  
+
   #TODO add relevant accessors if necessary
-  
+  attr_reader :average_rating, :city, :name
+
   def initialize(city, name)
     #TODO: implement constructor with relevant instance variables
+    @name = name
+    @city = city
+    @average_rating = "No visitor yet"
+    @ratings = []
   end
-  
+
+  def rate(new_rating)
+    @ratings << new_rating
+    @average_rating = @ratings.inject(:+)/@ratings.length
+  end
+
+  def self.filter_by_city(arg=[], city)
+    result = []
+    result << arg.pop {|resto| resto if resto.city == city}
+  end
+
   #TODO: implement #filter_by_city and #rate methods
-  
+
 end
+
