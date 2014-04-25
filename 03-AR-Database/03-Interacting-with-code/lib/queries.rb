f831612506f5a83109c75e9c7ea3d0051e3bcdda
sebastien <sebastien@ferre.io>
2014-04-25 13:09:13 +0200

    test passed

--- 03-AR-Database/03-Interacting-with-code/lib/queries.rb
+++ 03-AR-Database/03-Interacting-with-code/lib/queries.rb
@@ -6,20 +6,22 @@ db = SQLite3::Database.new(database_path)
 
 def number_of_rows(db, table_name)
   #TODO: count number of rows in table table_name
-  db.execute("SELECT COUNT(*) FROM table_name")
+  db.execute("SELECT COUNT(*) FROM #{table_name}").flatten.first
 end
 
 def sorted_artists(db)
   #TODO: return array of artists' names sorted alphabetically
-  db.execute("SELECT Name FROM artist ORDER BY Name")
+  db.execute("SELECT Name FROM artist ORDER BY Name").flatten
 end
 
 def love_tracks(db)
   #TODO: return array of love songs
-  db.execute("SELECT Name FROM track WHERE Name LIKE  '%love%'")
+  db.execute("SELECT Name FROM track WHERE Name LIKE  '%love%'").flatten
 end
 
 def long_tracks(db, min_length)
   #TODO: return tracks verifying: duration > min_length (minutes)
-  db.execute("SELECT Name FROM track WHERE Milliseconds > min_length * 60000")
+  db.execute("SELECT Name FROM track WHERE Milliseconds > #{min_length} * 60000").flatten
 end
+
+puts number_of_rows(db, "artist")
