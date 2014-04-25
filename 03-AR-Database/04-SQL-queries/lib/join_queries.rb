ab7318a1a6bdb73a3be33de54923c32908f16d01
sebastien <sebastien@ferre.io>
2014-04-25 13:09:52 +0200

    update rake files

--- 03-AR-Database/04-SQL-queries/lib/join_queries.rb
+++ 03-AR-Database/04-SQL-queries/lib/join_queries.rb
@@ -6,6 +6,9 @@ db = SQLite3::Database.new(DATABASE_PATH)
 
 def detailed_tracks(db)
   # TODO: return the list of tracks with their album and artist
+  db.execute("SELECT track.Name, album.Title, artist.name  FROM track
+    INNER JOIN album, artist
+    ON track.AlbumId == album.AlbumId AND album.ArtistId == artist.ArtistId")
 end
 
 def stats_on(db, category)
