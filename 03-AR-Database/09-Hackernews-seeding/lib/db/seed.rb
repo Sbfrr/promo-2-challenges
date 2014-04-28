6eba48fcd9933aadc19407ac912b25369b773bf5
sebastien <sebastien@ferre.io>
2014-04-28 15:53:41 +0200

    Seeding done

--- 03-AR-Database/09-Hackernews-seeding/lib/db/seed.rb
+++ 03-AR-Database/09-Hackernews-seeding/lib/db/seed.rb
@@ -1,12 +1,37 @@
 require_relative "../config/application"
 require_relative "../models/post"
 require_relative "../models/user"
+require 'open-uri'
+require 'nokogiri'
+require 'pp'
 
 # Seed you database with post data from hacker news
 # And fake users using Faker gem
 
 puts "Seeding database..."
 
-#TODO: Your code goes here 
+#TODO: Your code goes here
 # 1. create fake users
+for i in 0...5  do
+  User.create(name: Faker::Name.name, email: Faker::Internet.email)
+end
+
 # 2. Scrape news from https://news.ycombinator.com/ and associate posts to your existing users
+doc = Nokogiri::HTML(open('https://news.ycombinator.com/'))
+
+name = []
+url = []
+rating = []
+
+doc.css('.title a').each do |link|
+  url  << link['href']
+  name << link.inner_text
+end
+
+doc.css('.subtext span').each do |link|
+  rating << link.inner_text
+end
+
+for i in 0...name.length
+  Post.create(name: name[i], date: Time.now, source_url: url[i], rating: rating[i], user_id: 1 + rand(5))
+end
--- 03-AR-Database/09-Hackernews-seeding/lib/interface.rb
+++ 03-AR-Database/09-Hackernews-seeding/lib/interface.rb
@@ -2,4 +2,69 @@ require_relative 'config/application'
 require_relative 'models/post'
 require_relative 'models/user'
 
-#TODO: build your interface with login and CRUD tasks on the posts
+class HackerNewsUI
+
+  #TODO: build your interface with login and CRUD tasks on the posts
+  def self.ask_and_get(param)
+    puts "What is the #{param} of your piece of news?"
+    gets.chomp
+  end
+
+  def self.start_ui
+
+    while true
+      user = nil
+      wrong_email = true
+
+      # Identification loop
+      while user.nil? or wrong_email
+        print 'Please identify yourself: '
+        name = gets.chomp
+        user = User.find_by(name: "#{name}")
+
+        if user
+          puts "All right #{user.name}, enter your email:"
+          print '> '
+          email = gets.chomp
+          wrong_email = email != user.email
+          puts 'Incorrect email!!' if wrong_email
+        end
+      end
+
+      while true
+
+        puts "Hey #{user.name}, what do you want to do today? Enter <task_id>"
+        puts "1. Create a post"
+        puts "2. Read your posts"
+        puts "3. Delete all posts"
+        puts "4. Exit"
+
+        choice =  gets.chomp.to_i
+
+        case choice
+        when 1
+          name = HackerNewsUI.ask_and_get("name")
+          source_url = HackerNewsUI.ask_and_get("source url")
+          rating = HackerNewsUI.ask_and_get("rating")
+          #post = { name: name, source_url: source_url, date: Time.now, rating: rating }
+          #TODO: use ActiveRecord to add a new post for the user logged in!
+          Post.create(name: name, source_url: source_url, date: Time.now, rating: rating, user_id: user.id)
+        when 2
+          #TODO: use ActiveRecord to get all posts of the current user
+          posts = Post.all
+          posts.each do |post|
+            puts "#{post.id} -- #{post.name} - #{post.source_url} - #{post.rating} - #{User.find(post.user_id).name} \n "
+          end
+        when 3
+          #TODO: use ActiveRecord to delete all posts of current user
+          Post.destroy_all
+        when 4
+          break
+        end
+      end
+
+    end
+  end
+end
+
+HackerNewsUI.start_ui
