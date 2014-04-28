9cad48c070e48067aeaa706e9a14551ca1450286
sebastien <sebastien@ferre.io>
2014-04-28 16:23:38 +0200

    Validation tested

--- 03-AR-Database/10-Hackernews-validations/lib/interface.rb
+++ 03-AR-Database/10-Hackernews-validations/lib/interface.rb
@@ -1,3 +1,7 @@
 require_relative 'config/application'
 require './models/post'
 require './models/user'
+
+
+User.create(name: "polo", email: "polo@gmail.com")
+User.create(name: "polo", email: "polo@gmail.com")
